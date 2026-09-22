6ec56e9a46f0fe82f1145a8d722bfa1964fac8a8
dalebird.github.io <vihaan.sinha.17@gmail.com>
2026-09-22 21:35:56 +0100

    minor changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,11 +1,50 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.MecanumConfig;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.pedropathing.revhub.localizers.PinpointConfig;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.pedropathing.algorithm.Foresight;
+import com.pedropathing.algorithm.ForesightConfig;
+import com.pedropathing.revhub.drivetrains.Mecanum;
+import com.pedropathing.revhub.localizers.PinpointLocalizer;
 
 public class Constants {
     public static Follower create(HardwareMap h) {
-        // return new Follower(Drivetrain, Localizer, Foresight);
-        return null;
+        return new Follower
+                (new PinpointLocalizer(h,localizerConfig), new Mecanum(h, drivetrainConfig), new Foresight(foresightConfig));
     }
+
+    public static MecanumConfig drivetrainConfig =
+            new MecanumConfig(c -> {
+
+                c.frontLeftName.set("frontLeft");
+                c.backLeftName.set("backLeft");
+                c.frontRightName.set("frontRight");
+                c.backRightName.set("backRight");
+
+            });
+
+    public static PinpointConfig localizerConfig =
+            new PinpointConfig(c -> {
+
+                c.name.set("pinpoint");
+
+                // i need to put the actual values
+                c.xPodOffset.set(0.0);
+                c.yPodOffset.set(0.0);
+
+                c.xPodDirection.set(
+                        GoBildaPinpointDriver.EncoderDirection.FORWARD
+                );
+
+                c.yPodDirection.set(
+                        GoBildaPinpointDriver.EncoderDirection.FORWARD
+                );
+            });
+
+    public static ForesightConfig foresightConfig = new ForesightConfig(
+            c -> {}
+    );
 }
